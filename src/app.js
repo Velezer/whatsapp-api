a31664db2a775c27db427905530ab7833cd9d5c0
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-08 07:54:05 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,14 +13,15 @@ const io = socketIO(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    // handlePreflightRequest: (req, res) => {
-    //   res.writeHead(200, {
-    //     "Access-Control-Allow-Origin": "https://example.com",
-    //     "Access-Control-Allow-Methods": "GET,POST",
-    //     "Access-Control-Allow-Headers": "my-custom-header",
-    //     "Access-Control-Allow-Credentials": true
-    //   });
-    //   res.end();
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "https://example.com",
+        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Headers": "my-custom-header",
+        "Access-Control-Allow-Credentials": true
+      });
+      res.end();
+    },
     // allowedHeaders: ["my-custom-header"],
     credentials: true
   },
