219da1b3f2e7b5d4f8b51164cbcc6321263d2c63
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-08 07:55:53 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const io = socketIO(server, {
     methods: ["GET", "POST"],
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
-        "Access-Control-Allow-Origin": "https://example.com",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,POST",
         "Access-Control-Allow-Headers": "my-custom-header",
         "Access-Control-Allow-Credentials": true
