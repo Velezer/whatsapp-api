50c691588b417d6d49a850d66b158fc413a0fe4c
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-09 12:48:33 +0700

    Update validator.js

--- src/validator.js
+++ src/validator.js
@@ -5,7 +5,7 @@ const validateReqSendMessage = [
     body(`message`).notEmpty()
 ]
 const validateReqSendMessages = [
-    body(`numbers`,`can't convert string to array`).notEmpty().isString().toArray(),
+    body(`numbers`,`can't convert string to array`).toArray(),
     body(`numbers`, `array min length is 1`).notEmpty().isArray({min:1}),
     body(`numbers.*`,`not a mobile phone`).isMobilePhone(),
     body(`message`).notEmpty()
