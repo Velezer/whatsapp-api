805899f7f292097352a381f18cde21ae62ac7417
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-09 15:43:05 +0700

    Update validator.js

--- src/validator.js
+++ src/validator.js
@@ -2,9 +2,11 @@ const { body } = require('express-validator');
 
 
 const validateReqSendMessages = [
+    body(`numbers`,`numbers is empty`).notEmpty(),
     body(`numbers`,`can't convert to array`).toArray(),
-    body(`numbers`, `array min length is 1`).notEmpty().isArray({min:1}),
+    body(`numbers`, `numbers array min length is 1`).isArray({min:1}),
     body(`numbers.*`,`not a mobile phone`).isMobilePhone(),
+
     body(`message`).notEmpty()
 ]
 
