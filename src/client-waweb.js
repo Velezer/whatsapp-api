be25372c8350439ed1d823f05ed0114b6161f00c
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-13 08:10:44 +0700

    Update client-waweb.js

--- src/client-waweb.js
+++ src/client-waweb.js
@@ -63,13 +63,13 @@ class ClientWaweb extends Client {
                 headless: true,
                 args: [
                     '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage',
+                    // '--disable-setuid-sandbox',
+                    // '--disable-dev-shm-usage',
                     '--disable-accelerated-2d-canvas',
-                    '--no-first-run',
-                    '--no-zygote',
-                      '--single-process', // <- this one doesn't works in Windows
-                    '--disable-gpu'
+                    // '--no-first-run',
+                    // '--no-zygote',
+                    //   '--single-process', // <- this one doesn't works in Windows
+                    // '--disable-gpu'
                 ],
             },
             session: sessionData ? sessionData.session : null
