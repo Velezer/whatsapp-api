083ef0dc9d8e3441fd9dfdd6ed602bd6b0b36303
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-15 18:00:56 +0700

    Update create.js

--- src/handler/user/create.js
+++ src/handler/user/create.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator');
 const { UserModel } = require('../../model/user')
 
 module.exports = async (req, res) => {
-    console.log(`insertContacts`)
+    console.log(`user-create`)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -12,8 +12,9 @@ module.exports = async (req, res) => {
 
     const { user, password } = req.body
 
-    const userData = new UserModel({ user, password })
+    const userData = new UserModel({ user, password, contacts: [] })
     const result = await new userData.save()
+
     res.status(201).json({
         user,
         message: `successfully created`,
