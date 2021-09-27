1958fe6fc61739eb257ef716c514fba0907cf4ad
Arief Syaifuddin <asvelezer@gmail.com>
2021-09-27 21:02:20 +0700

    post to delete

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const Handler = require('../handler/handler')
 const { Validator } = require('../validation/validator')
 
 router.post('/', Validator.user.common, Handler.user.create)
-router.post('/', Validator.user.common, Handler.user.delete)
+router.delete('/', Validator.user.common, Handler.user.delete)
 router.put('/contacts', Validator.user.contact, Handler.user.pushContact)
 router.delete('/contacts', Validator.user.contact, Handler.user.deleteContact)
 router.get('/contacts', Validator.user.common, Handler.user.showContacts)
