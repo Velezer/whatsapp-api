3a10b0ea991f4ee35e1beea72bd5cb4ab221b7aa
Arief Syaifuddin <asvelezer@gmail.com>
2021-09-28 11:00:18 +0700

    some comment

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,11 @@ const router = require("express").Router()
 const Handler = require('../handler/handler').user
 const { Validator } = require('../validation/validator')
 
+// user acccount
 router.post('/', Validator.user.common, Handler.create)
 router.delete('/', Validator.user.common, Handler.delete)
+
+// user's contacts
 router.put('/contacts', Validator.user.contact, Handler.pushContact)
 router.delete('/contacts', Validator.user.contact, Handler.deleteContact)
 router.get('/contacts', Validator.user.common, Handler.showContacts)
