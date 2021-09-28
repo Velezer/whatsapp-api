11bfda74d656df08c7adeb4c951bc39b541077f3
Arief Syaifuddin <asvelezer@gmail.com>
2021-09-28 08:30:38 +0700

    fix logic

--- src/routes/waweb.js
+++ src/routes/waweb.js
@@ -2,11 +2,10 @@ const router = require("express").Router()
 const Handler = require('../handler/handler').waweb
 const { Validator } = require('../validation/validator')
 
-router.use(Handler.getClientMiddleware)
 
-router.post('/send-message', Validator.waweb.sendMessage, Handler.sendMessage)
-router.post('/send-media', Validator.waweb.sendMedia, Handler.sendMedia)
-router.post('/get-contacts', Validator.user.common, Handler.getContacts)
+router.post('/send-message', Validator.waweb.sendMessage, Handler.getClientMiddleware, Handler.sendMessage)
+router.post('/send-media', Validator.waweb.sendMedia, Handler.getClientMiddleware, Handler.sendMedia)
+router.post('/get-contacts', Validator.user.common, Handler.getClientMiddleware, Handler.getContacts)
 
 
 module.exports = router
